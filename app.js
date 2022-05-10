60d126f85808110814a8db6989172d714a7b78c6
Moonimoon93 <harrygularski@gmail.com>
2022-05-10 15:33:41 +0900

    Large Update! 1. Finished Home page.     a. drop down animation     b. navigation buttons are functional with animation as well. 2. Working on About page     a. Going to present myself with my accomplishments.

---
+++ app.js
@@ -0,0 +1,33 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+function PageTransitions(){
+    //whenever a button is clicked, except the clicked button
+    //any other active-btn classes have to be removed.
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
+            let currentActiveBtn = document.querySelectorAll('.active-btn');
+            currentActiveBtn[0].className = currentActiveBtn[0].className.replace('active-btn','');
+            this.className += ' active-btn'
+        })
+    }
+    //section transition as buttons are clicked 
+    allSections.addEventListener('click', (event)=>{
+        const id = event.target.dataset.id;
+        if(id){
+            sectBtns.forEach((btn)=>{
+                btn.classList.remove('active')
+            })
+            event.target.classList.add('active')
+
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+}
+PageTransitions();
